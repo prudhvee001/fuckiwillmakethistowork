2dca2734ebab4280e608c6cc315de7c8e164361a
prudhvee001 <85451196+prudhvee001@users.noreply.github.com>
2025-04-27 17:11:04 +0530

    Delete terraform directory

--- terraform/modules/alb/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "alb_dns_name" {
-  description = "DNS Name of the ALB"
-  value       = aws_lb.alb.dns_name
-}
--- terraform/modules/eks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  description = "Project Name"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "Subnets for EKS Cluster"
-  type        = list(string)
-}
